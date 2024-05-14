54784b285c4e22a368076f7f79e1032b679d936c
egooktafanda97 <egooktafanda1097@gmail.com>
2024-05-14 11:53:21 +0700

    up

---
+++ application/views/Page/WhastApps/show-wa.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Koneksikan WhastApp</h4>
+                <div class="table-responsive">
+                    <div class="p-3" style="display: flex; justify-content: center; align-items: center;">
+                        <img src="" style="width: 300px; height: 300px;" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
